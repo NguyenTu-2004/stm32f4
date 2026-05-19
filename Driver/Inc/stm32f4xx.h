a3d72904613fa45e5c18dbe56e697dd6c6256e5b
NguyenTu <anhtu23082004@gmail.com>
2026-05-19 22:12:38 +0700

    Add SPI reg bits

--- Driver/Inc/stm32f4xx.h
+++ Driver/Inc/stm32f4xx.h
@@ -2,6 +2,7 @@
 #define _STM32F4XX_H
 
 #include <stdint.h>
+#include <stddef.h>
 
 //NVIC ISERx register addresses
 #define NVIC_ISER0           ((volatile uint32_t*)0xE000E100)
@@ -314,5 +315,41 @@ typedef struct
 #define IRQ_NO_EXTI4       10
 #define IRQ_NO_EXTI9_5     23
 #define IRQ_NO_EXTI15_10   40
+
+//SPI CR1
+#define SPI_CR1_CPHA       0
+#define SPI_CR1_CPOL       1
+#define SPI_CR1_MSTR       2
+#define SPI_CR1_BR         3
+#define SPI_CR1_SPE        6
+#define SPI_CR1_LSBFIRST   7
+#define SPI_CR1_SSI        8
+#define SPI_CR1_SSM        9
+#define SPI_CR1_RXONLY     10
+#define SPI_CR1_DFF        11
+#define SPI_CR1_CRCNEXT    12
+#define SPI_CR1_CRCEN      13
+#define SPI_CR1_BIDIOE     14
+#define SPI_CR1_BIDIMODE   15
+
+//SPI CR2
+#define SPI_CR2_RXDMAEN    0
+#define SPI_CR2_TXDMAEN    1
+#define SPI_CR2_SSOE       2
+#define SPI_CR2_FRF        4
+#define SPI_CR2_ERRIE      5
+#define SPI_CR2_RXNEIE     6
+#define SPI_CR2_TXEIE      7
+
+//SPI SR
+#define SPI_SR_RXNE         0
+#define SPI_SR_TXE          1
+#define SPI_SR_CHSIDE       2
+#define SPI_SR_UDR          3
+#define SPI_SR_CRCERR       4
+#define SPI_SR_MODF         5
+#define SPI_SR_OVR          6
+#define SPI_SR_BSY          7
+#define SPI_SR_FRE          8
 
 #endif /* _STM32F4XX_H */
